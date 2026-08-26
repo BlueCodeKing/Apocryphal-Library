ad95c433ff16e7baf907c746240599c18e461721
BlueCodeKing <105813725+BlueCodeKing@users.noreply.github.com>
2026-08-26 15:28:14 -0500

    Add files via upload

---
+++ scripts/header and navigation.js
@@ -0,0 +1,7 @@
+function navigationOpen() {
+    document.getElementById("navigation").style.right = ("0px");
+}
+
+function navigationClose() {
+    document.getElementById("navigation").style.right = ("-100%");
+}
