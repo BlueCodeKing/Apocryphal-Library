30bacb4d773f7aa59511fed915fc67085e3d041b
BlueCodeKing <105813725+BlueCodeKing@users.noreply.github.com>
2026-09-17 12:05:06 -0500

    Delete scripts/header and navigation.js

--- scripts/header and navigation.js
+++
@@ -1,7 +0,0 @@
-function navigationOpen() {
-    document.getElementById("navigation").style.right = ("0px");
-}
-
-function navigationClose() {
-    document.getElementById("navigation").style.right = ("-100%");
-}
