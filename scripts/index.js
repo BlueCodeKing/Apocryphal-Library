7bae6da1fdd2ee6edd66d57ce47ef984fdbafe6b
BlueCodeKing <105813725+BlueCodeKing@users.noreply.github.com>
2026-09-19 15:14:47 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,20 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("navigation").style.right = ("-350px");
 }
+
+// MORE INFO (for chapter pages)
+
+function moreInfo() {
+    const info = document.getElementById("info");
+    const moreInfoText = document.getElementById("moreInfo");
+
+    if (info.style.display === "none" || info.style.display === "") {
+        info.style.display = "block";
+        moreInfoText.textContent = "Hide More Info";
+        moreInfoText.style.backgroundColor = "dodgerblue";
+    } else {
+        info.style.display = "none";
+        moreInfoText.textContent = "Click for More Info";
+        moreInfoText.style.backgroundColor = "";
+    }
+}
